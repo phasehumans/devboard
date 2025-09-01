1696129b85367a1b32b01d1d84cf8c73b94016d4
chaitanya <dev.chaitanyasonawane@gmail.com>
2025-09-01 19:22:13 +0530

    db and api res/err

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import app from "./app.js";
+import dotenv from "dotenv"
+import connectDB from "./db/db.connect.js";
+
+dotenv.config({
+    path: "./.env",
+})
+
+
+const PORT= process.env.PORT || 8000
+
+connectDB()
+    .then(()=>{
+        app.listen(PORT, ()=>{
+            console.log(`server is running on ${PORT} PORT`);
+            
+        })
+    })
+    .catch(()=> {
+        console.error("monogodb connection failed" , error)
+        process.exit(1)
+    })
