1ba83935bc901fa3f5fd5d4925d8ffb7912967d4
chaitanya <dev.chaitanyasonawane@gmail.com>
2025-09-08 18:28:42 +0530

    models completed

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -68,11 +68,60 @@ const UserSchema= new Schema({
     }
 },{timestamps: true})
 
-export const User= mongoose.model("User", UserSchema)
 
+// hook
 UserSchema.pre("save", async function (next){
     if(!this.isModified("password")) return next();
     this.password= await bcrypt.hash(this.password, 10)
     next();
 })
 
+
+// methods
+
+UserSchema.methods.isPassCorrect= async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+UserSchema.methods.generateAccessToken= function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username
+        },
+
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
+
+    )
+}
+
+UserSchema.methods.generateRefreshToken= function(){
+    return jwt.sign(
+        {
+            _id: this.id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {expiresIn: process.env.REFRESH_TOKEN_EXPIRY}
+    )
+}
+
+userSchema.methods.generateTemporaryToken = function () {
+  // This token should be client facing
+  // for example: for email verification unHashedToken should go into the user's mail
+  const unHashedToken = crypto.randomBytes(20).toString("hex");
+
+  // This should stay in the DB to compare at the time of verification
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(unHashedToken)
+    .digest("hex");
+  // This is the expiry time for the token (20 minutes)
+  const tokenExpiry = Date.now() + 20 * 60 * 1000; // 20 minutes;
+
+  return { unHashedToken, hashedToken, tokenExpiry };
+};
+
+
+export const User= mongoose.model("User", UserSchema)
