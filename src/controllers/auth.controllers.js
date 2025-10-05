ef477001bad663d2b104cd71ca50fede5e675c91
chaitanya <dev.chaitanyasonawane@gmail.com>
2025-10-06 00:16:54 +0530

    updated controllers

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,9 +1,46 @@
+import { body } from "express-validator";
 import { asyncHandler } from "../utils/async-handler.js";
+import { userRegistrationValidator } from "../validators/index.js";
+import {User} from "../models/user.models.js"
+
 const registerUser = asyncHandler(async (req, res) => {
+  // get data
   const { email, username, password, role } = req.body;
 
   //validation
-  registrationValidation(body);
+  userRegistrationValidator(body)
+
+  // check existingUser
+  const existingUser= await User.findOne({email})
+
+  if(existingUser){
+    
+  }
 });
 
-export { registerUser };
+
+const verifyUser= asyncHandler(async (req, res) =>{
+
+})
+
+const loginUser= asyncHandler(async (req, res) =>{
+
+})
+
+const getMe= asyncHandler(async (req, res) =>{
+
+})
+
+const logoutUser= asyncHandler(async (req, res) =>{
+
+})
+
+const forgotPassword= asyncHandler(async (req, res) =>{
+
+})
+
+const resetPassword= asyncHandler(async (req, res) => {
+
+})
+
+export { registerUser, verifyUser, loginUser, getMe, logoutUser, forgotPassword, resetPassword};
